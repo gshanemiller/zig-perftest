a94cec66176719be0cbfa966635f25d5550f9ae5
smiller <gshanemiller6@gmail.com>
2023-03-16 23:22:13 +0000

    remove junk printfs

--- src/ib/ib_device.c
+++ src/ib/ib_device.c
@@ -12,9 +12,9 @@ int ice_ib_find_device(const char *deviceName, struct ibv_device **device) {
 
   // Request device list and iterate until found
   int deviceCount = -1;
-  struct ibv_device **deviceList = ibv_get_device_list(&deviceCount);
+  ibv_get_device_list(&deviceCount);
 
-  printf("got %p %d\n", deviceList, deviceCount);
+  printf("got %d devices expect (2) devices\n", deviceCount);
   
   if (deviceCount<=0) {
     return (ICE_IB_ERROR_NO_DEVICE);
