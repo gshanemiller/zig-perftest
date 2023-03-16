2874ddd25a5c52d5d6949b0e7af4ed286e50ebf5
smiller <gshanemiller6@gmail.com>
2023-03-16 21:39:26 +0000

    upgrade to edge zig

--- build.zig
+++ build.zig
@@ -1,26 +1,26 @@
 const std = @import("std");
 
-pub fn build(b: *std.build.Builder) void {
-    // Standard target options allows the person running `zig build` to choose
-    // what target to build for. Here we do not override the defaults, which
-    // means any target is allowed, and the default is native. Other options
-    // for restricting supported target set are available.
+pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
 
-    // Standard release options allow the person running `zig build` to select
-    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
-    const mode = b.standardReleaseOptions();
+    // declare getopt module
+    const getoptModule = std.Build.CreateModuleOptions {
+      .source_file = .{ .path = "libs/getopt/getopt.zig" },
+    };
+    const getoptModuleObj = b.addModule("getopt", getoptModule);
 
-    const exe = b.addExecutable("zig-perftest", "src/main.zig");
+    const exe = b.addExecutable(.{
+        .name = "zig-perftest",
+        .root_source_file = .{ .path = "src/main.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
 
-    std.debug.print("before {any}\n", .{exe.linkage});
-    exe.linkage = std.build.LibExeObjStep.Linkage.dynamic;
-    std.debug.print("after {any}\n", .{exe.linkage});
+    exe.verbose_link = true;
 
-    exe.addPackagePath("getopt", "libs/getopt/getopt.zig");
-
-    exe.setTarget(target);
-    exe.setBuildMode(mode);
+    // Add getopt module to executable
+    exe.addModule("getopt", getoptModuleObj);
 
     // C compliation options
     const cflags = [_][]const u8{
@@ -28,7 +28,6 @@ pub fn build(b: *std.build.Builder) void {
       "-g",
       "-O2",
       "-Wall",
-//    "-Werror",
       "-march=native",
       "-std=c11",
     };
@@ -45,7 +44,7 @@ pub fn build(b: *std.build.Builder) void {
     exe.addIncludePath("/usr/include/infiniband");
     exe.addIncludePath("/usr/include/x86_64-linux-gnu");
 
-    // C linker paths
+    // C dynamic linker paths
     exe.addLibraryPath("/usr/lib/x86_64-linux-gnu");
 
     // C source code
@@ -55,6 +54,7 @@ pub fn build(b: *std.build.Builder) void {
     exe.linkLibC();
 
     // And link in dependent IB libraries
+    exe.linkSystemLibrary("m");
     exe.linkSystemLibrary("mlx5");
     exe.linkSystemLibrary("efa");
     exe.linkSystemLibrary("rdmacm");
@@ -63,30 +63,51 @@ pub fn build(b: *std.build.Builder) void {
     exe.linkSystemLibrary("pthread");
     exe.linkSystemLibrary("nl-route-3");
     exe.linkSystemLibrary("nl-3");
+    exe.linkSystemLibrary("udev");
 
-    std.debug.print("before {any}\n", .{exe.linkage});
-    exe.pie = true;
-    exe.force_pic = true;
-    exe.verbose_link = true;
-    exe.linkage = std.build.LibExeObjStep.Linkage.dynamic;
-    std.debug.print("after {any}\n", .{exe.linkage});
+    // exe.pie = true;
+    // exe.force_pic = true;
+    // exe.rdynamic = true;
+    // exe.linkage = std.build.LibExeObjStep.Linkage.dynamic;
 
-    // build & install
+    // This declares intent for the executable to be installed into the
+    // standard location when the user invokes the "install" step (the default
+    // step when running `zig build`).
     exe.install();
 
+    // This *creates* a RunStep in the build graph, to be executed when another
+    // step is evaluated that depends on it. The next line below will establish
+    // such a dependency.
     const run_cmd = exe.run();
+
+    // By making the run step depend on the install step, it will be run from the
+    // installation directory rather than directly from within the cache directory.
+    // This is not necessary, however, if the application depends on other installed
+    // files, this ensures they will be present and in the expected location.
     run_cmd.step.dependOn(b.getInstallStep());
+
+    // This allows the user to pass arguments to the application in the build
+    // command itself, like this: `zig build run -- arg1 arg2 etc`
     if (b.args) |args| {
         run_cmd.addArgs(args);
     }
 
+    // This creates a build step. It will be visible in the `zig build --help` menu,
+    // and can be selected like this: `zig build run`
+    // This will evaluate the `run` step rather than the default, which is "install".
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
 
-    const exe_tests = b.addTest("src/main.zig");
-    exe_tests.setTarget(target);
-    exe_tests.setBuildMode(mode);
+    // Creates a step for unit testing.
+    const exe_tests = b.addTest(.{
+        .root_source_file = .{ .path = "src/main.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
 
+    // Similar to creating the run step earlier, this exposes a `test` step to
+    // the `zig build --help` menu, providing a way for the user to request
+    // running the unit tests.
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&exe_tests.step);
 }
